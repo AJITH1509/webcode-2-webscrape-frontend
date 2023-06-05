65ae05f9ef40f850ffaafaef07b15aad00175845
AJITH1509 <ajithmuthu799@gmail.com>
2023-06-05 16:51:10 +0530

    https to http

--- global.js
+++ global.js
@@ -1,3 +1,3 @@
 export const API =
-  "http://webcode-2-webscraping-backend.vercel.app/scrape-amazon-product";
+  "https://webcode-2-webscraping-backend.vercel.app/scrape-amazon-product";
 // export const API = "http://localhost:3000/scrape-amazon-product";
